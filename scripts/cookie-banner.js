d4f81a43ef7ae1fed44f98a69caaf8212f2bdab5
Pranav Kuruvittavil <pranav123reeba@gmail.com>
2026-04-22 12:02:23 +0530

    Update script source in cookie-banner.js to the new CDN link for CookieYes integration.

--- scripts/cookie-banner.js
+++ scripts/cookie-banner.js
@@ -6,6 +6,6 @@
   var script = document.createElement("script");
   script.id = "cookieyes";
   script.type = "text/javascript";
-  script.src = "https://cdn-cookieyes.com/client_data/08edeacb8b40ff1efe0e676d78e9bc1e/script.js";
+  script.src = "https://cdn-cookieyes.com/client_data/5112d75d24ca593b58cf8ff84c94b373/script.js";
   document.head.appendChild(script);
 })();
