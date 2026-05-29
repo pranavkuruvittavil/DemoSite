42bc07e033945c5af1b85aa220a8d00035b39db6
Pranav Kuruvittavil <pranav123reeba@gmail.com>
2026-05-29 13:04:00 +0530

    Update CookieYes privacy policy script source in cookie-banner.js to a new CDN link for improved functionality.

--- scripts/cookie-banner.js
+++ scripts/cookie-banner.js
@@ -6,6 +6,6 @@
   var script = document.createElement("script");
   script.id = "cookieyes";
   script.type = "text/javascript";
-  script.src = "https://cdn-mojoview.uk/client_data/c5947d4e621fa86dd2a1f31867eb0768/script.js";
+  script.src = "https://cdn-cookieyes.com/client_data/867a39b0688bdb20a25827873e419df0/script.js";
   document.head.appendChild(script);
 })();
