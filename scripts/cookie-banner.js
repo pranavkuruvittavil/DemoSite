b88aa5dec0a40bd9039f02e72db1287e2f1f57cd
Pranav Kuruvittavil <pranav.k@mozilor.com>
2026-08-03 10:25:11 +0530

    Update CookieYes privacy policy script source in cookie-banner.js to a new production CDN link for improved functionality.

--- scripts/cookie-banner.js
+++ scripts/cookie-banner.js
@@ -6,6 +6,6 @@
   var script = document.createElement("script");
   script.id = "cookieyes";
   script.type = "text/javascript";
-  script.src = "https://cdn-mojoview.uk/client_data/76f90690c7905f06baa8876bc7c4f0ac/script.js";
+  script.src = "https://cdn-mojoview.uk/client_data/6e77a596f9a1aac668d1bf42d1ebb015/script.js";
   document.head.appendChild(script);
 })();
